a0792784660ff2669c082cc4a7573422362dfac9
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-09 18:25:23 +0530

    Update service_account.tf

--- service_account.tf
+++ service_account.tf
@@ -1,7 +1,7 @@
 #tag 
 module "service_account576" {  
 source = "./modules/service_account"
-project_roles = ["expanded-origin-316105=>roles/viewer"]
+project_roles = ["secops-316111"]
 display_name  = var.display_name
 account_id    = var.account_id 
 project_id = "secops-316111"
