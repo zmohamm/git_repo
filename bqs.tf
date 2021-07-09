036498474d9b7cd7a46ca2f7fd668dd3ce413276
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-09 16:20:09 +0530

    committing files

---
+++ bqs.tf
@@ -0,0 +1,24 @@
+#tag 
+module "big_query1" {  
+source = "./modules/bqs"
+dataset_location = var.location 
+project_id = "secops-316111" 
+dataset_id = "secops-316111_Secops843"
+}
+
+#tag 
+module "big_query2" {  
+source = "./modules/bqs"
+dataset_location = var.location 
+project_id = "secops-316111" 
+dataset_id = "secops-316111_Secops768"
+}
+
+#tag 
+module "big_query1" {  
+source = "./modules/bqs"
+dataset_location = var.location 
+project_id = "secops-316111" 
+dataset_id = "secops-316111_Secops576"
+}
+
