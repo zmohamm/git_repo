7704e7054a6eba22b59a56c3718f15b32345ebb6
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-09 17:17:49 +0530

    Update vm_instance.tf

--- vm_instance.tf
+++ vm_instance.tf
@@ -4,7 +4,7 @@ source = "./modules/vm_instance"
 project_id = var.project_id
 location = var.location
 zone = var.zone 
-machines_typ = "e2-standard-2(2 vCPU, 8 GB memory)" 
+machines_typ = "e2-standard" 
 name = "vminstance362"
 }
 
