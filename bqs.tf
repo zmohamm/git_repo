57c9066ec46b8e5b112229fe5e6fc14d0ae06d20
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-12 09:54:34 +0530

    committing files

--- bqs.tf
+++ bqs.tf
@@ -46,3 +46,11 @@ project_id = "secops-316111"
 dataset_id = "secops_316111_Secops646"
 }
 
+#tag 
+module "big_query606" {  
+source = "./modules/bqs"
+dataset_location = var.location 
+project_id = "secops-316111" 
+dataset_id = "secops_316111_Secops603"
+}
+
