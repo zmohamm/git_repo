6e4aeba3558face79ee2dd8646898cf72c0a2536
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-14 22:04:45 +0530

    Delete service_account.tf

--- service_account.tf
+++
@@ -1,9 +0,0 @@
-#tag 
-module "service_account550" {  
-source = "./modules/service_account"
-project_roles = ["secops-316111=>roles/viewer"]
-display_name  = var.display_name
-account_id    = var.account_id 
-project_id = "secops-316111"
-}
-
