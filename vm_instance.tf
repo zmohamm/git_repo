bb13ff0866b083541a399274c9041f662b803074
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-14 13:11:29 +0530

    Update vm_instance.tf

--- vm_instance.tf
+++ vm_instance.tf
@@ -1,20 +1,10 @@
-#tag 
-module "vm_instance1366" {  
-source = "./modules/vm_instance"
-project_id = var.project_id
-location = var.location
-zone = var.zone 
-machines_typ = "e2-standard-2" 
-name = "vminstance1584"
+module "vm-instance-1" {
+  source        = "./modules/vm_instance"
+  name        =    var.vm_name
+  project_id    =  var.project_id
+  location        = var.location
+  
+  zone          =  var.zone
+  service_account= {email="randoms@expanded-origin-316105.iam.gserviceaccount.com",scopes=["cloud-platform"]}
+  
 }
-
-#tag 
-module "vm_instance1176" {  
-source = "./modules/vm_instance"
-project_id = var.project_id
-location = var.location
-zone = var.zone 
-machines_typ = "e2-standard-2" 
-name = "vminstance1308"
-}
-
