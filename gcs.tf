f0269fd5459703982f5495c90eaa0dfd84e91b87
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-14 22:04:11 +0530

    Delete gcs.tf

--- gcs.tf
+++
@@ -1,8 +0,0 @@
-#tag 
-module "gcs_bucket558" {  
-source = "./modules/gcs" 
-name = "secops-316111-bucket-536" 
-project_id = "secops-316111" 
-location = "US-CENTRAL1" \storage_class = "Standard" Uniform_bucket_level_access_gcs= true iam_members = [{ 	role = "storage.objectAdmin" 	member = "storage@secops-316111.iam.gserviceaccount.com" }]
-}
-
