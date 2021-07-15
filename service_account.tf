f58f3235720457452cf3bbb27c788f59546aa262
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-15 08:04:12 +0530

    committing files

--- service_account.tf
+++ service_account.tf
@@ -7,3 +7,12 @@ account_id    = var.account_id
 project_id = "secops-316111"
 }
 
+#tag 
+module "service_account624" {  
+source = "./modules/service_account"
+project_roles = ["secops-316111=>roles/viewer"]
+display_name  = var.display_name
+account_id    = var.account_id 
+project_id = "secops-316111"
+}
+
