9f9324cd264105de5e11686a4f59320390b9f3d4
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-15 08:03:38 +0530

    committing files

--- vm_instance.tf
+++ vm_instance.tf
@@ -9,3 +9,14 @@ zone = "us-central1-a"
 service_account= {email= "jenkins@secops-316111.iam.gserviceaccount.com",scopes=["cloud-platform"]}
 }
 
+#tag 
+module "vm_instance508" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location 
+machines_typ = "e2-standard-2" 
+name = "vminstance189" 
+zone = "us-central1-a" 
+service_account= {email= "jenkins@secops-316111.iam.gserviceaccount.com",scopes=["cloud-platform"]}
+}
+
