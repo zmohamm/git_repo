314e04d3035d65802b8c87e28422bc5a0999668c
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-15 18:42:01 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,22 +1,22 @@
 provider "template" {
-  version = "~>2.1.2"  #### provider version nor needed anymore with the latest terraform update    #####
+  #version = "~>2.1.2"  #### provider version nor needed anymore with the latest terraform update    #####
 }
 
 provider "google" {
-  version = "=3.38.0"
+  #version = "=3.38.0"
   project = var.project_id
   
 }
 
 provider "google-beta" {
-  version ="=3.38.0"
+  #version ="=3.38.0"
   project = var.project_id
   region  = var.location
   #zone    = var.zone
 }
 
 terraform {
- # required_version = "=1.0.0"
+  required_version = "=1.0.0"
   backend "gcs" {
     bucket = "gcs-jenkins-tf-backend"
     prefix = "terraform/statefile"
