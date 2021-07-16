11ef619bb22c78bdbdfb5b96593a516bc4411a9f
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-16 18:00:48 +0530

    committing files

--- gcs.tf
+++ gcs.tf
@@ -40,3 +40,17 @@ member = "serviceAccount:storage@secops-316111.iam.gserviceaccount.com"
 }]
 }
 
+#tag 
+module "gcs_bucket6271" {  
+source = "./modules/gcs" 
+name = "secops-316111-bucket-6275" 
+project_id = "secops-316111" 
+location = "US-CENTRAL1" 
+storage_class = "Standard" 
+uniform_bucket_level_access_gcs = true 
+iam_members = [{ 
+role = "roles/storage.objectAdmin" 
+member = "serviceAccount:storage@secops-316111.iam.gserviceaccount.com" 
+}]
+}
+
