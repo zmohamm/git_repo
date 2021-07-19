fc2317d98940e74a79f1c709cfaa06d31b902be7
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-19 11:04:33 +0530

    committing files

--- vm_instance.tf
+++ vm_instance.tf
@@ -9,3 +9,14 @@ zone = "us-central1-a"
 service_account= {email= "94609799097-compute@developer.gserviceaccount.com",scopes=["cloud-platform"]}
 }
 
+#tag 
+module "vm_instance3425" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location 
+machines_typ = "e2-standard-2" 
+name = "vminstance6564" 
+zone = "us-central1-a" 
+service_account= {email= "94609799097-compute@developer.gserviceaccount.com",scopes=["cloud-platform"]}
+}
+
