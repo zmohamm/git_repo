51f5e2f81c2c1b0a2629faf3bbcc7a790a941f4d
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-08-10 13:44:41 +0530

    committing files

---
+++ gcs.tf
@@ -0,0 +1,14 @@
+#tag 
+module "gcs_bucket5241" {  
+source = "./modules/gcs" 
+name = "gcp-cube-321106-bucket-4244" 
+project_id = "gcp-cube-321106" 
+location = "US-CENTRAL1" 
+storage_class = "Standard" 
+uniform_bucket_level_access_gcs = true 
+iam_members = [{ 
+role = "roles/Storage Object Creator" 
+member = "serviceAccount:ind-coe-infra@gcp-cube-321106.iam.gserviceaccount.com" 
+}]
+}
+
