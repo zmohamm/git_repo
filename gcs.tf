7645514231c03c48b3fc1a05a0783f60a57308e4
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-08-10 14:00:45 +0530

    Update gcs.tf

--- gcs.tf
+++ gcs.tf
@@ -7,7 +7,7 @@ location = "US-CENTRAL1"
 storage_class = "Standard" 
 uniform_bucket_level_access_gcs = true 
 iam_members = [{ 
-role = "roles/storage.ObjectCreator" 
+role = "roles/storage.objectCreator" 
 member = "serviceAccount:ind-coe-infra@gcp-cube-321106.iam.gserviceaccount.com" 
 }]
 }
