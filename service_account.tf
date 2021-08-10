b9bbdce8303c9f86e926ab69b1347981106f5d49
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-08-10 12:30:15 +0530

    Delete service_account.tf

--- service_account.tf
+++
@@ -1,9 +0,0 @@
-#tag 
-module "service_account6931" {  
-source = "./modules/service_account"
-project_roles = ["gcp-cube-321106=>roles/viewer"]
-display_name  = var.display_name
-account_id    = var.account_id 
-project_id = "gcp-cube-321106"
-}
-
