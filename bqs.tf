01982a894d89388077fccecdf8baa923b692856c
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-08-11 08:04:42 +0530

    committing files

--- bqs.tf
+++ bqs.tf
@@ -6,3 +6,11 @@ project_id = "gcp-cube-321106"
 dataset_id = "gcp_cube_321106_Sample5015"
 }
 
+#tag 
+module "big_query8402" {  
+source = "./modules/bqs"
+dataset_location = var.location 
+project_id = "gcp-cube-321106" 
+dataset_id = "gcp_cube_321106_Sample6764"
+}
+
