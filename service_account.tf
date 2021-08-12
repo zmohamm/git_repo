ec0f8ea307890fc649e7db445ec57fd0e1205dfe
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-08-12 08:03:54 +0530

    committing files

--- service_account.tf
+++ service_account.tf
@@ -7,3 +7,12 @@ account_id    = var.account_id
 project_id = "gcp-cube-321106"
 }
 
+#tag 
+module "service_account6114" {  
+source = "./modules/service_account"
+project_roles = ["gcp-cube-321106=>roles/viewer"]
+display_name  = var.display_name
+account_id    = var.account_id 
+project_id = "gcp-cube-321106"
+}
+
