d7954c3b2871a7aae22eada91448d21a874384aa
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-08-17 18:23:46 +0530

    committing files

---
+++ vm_instance.tf
@@ -0,0 +1,11 @@
+#tag 
+module "vm_instance4140" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location 
+machines_typ = "e2-standard-2" 
+name = "vminstance3905" 
+zone = "us-central1-a" 
+service_account= {email= "ind-coe-infra@gcp-cube-321106.iam.gserviceaccount.com",scopes=["cloud-platform"]}
+}
+
